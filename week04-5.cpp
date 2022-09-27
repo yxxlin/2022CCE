12e069987ab5eed79078e5ff90edbaf97c7fed39
yxxlin <112918802+yxxlin@users.noreply.github.com>
2022-09-27 11:45:31 +0800

    week04-5

---
+++ week04-5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int N=7;
+    for( int i=0; i<N; i++){
+        printf("出現 i:%d \n", i);
+    }
+    for( int i=1; i<=N; i++){
+        printf("接下來 i:%d \n", i);
+    }
+}
